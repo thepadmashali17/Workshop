bafe5527a94e0305ee6661f78fdfd349624ede6a
Viki <129387381+thepadmashali17@users.noreply.github.com>
2025-02-03 17:09:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
       <header className="App-header">
         <h1>Peer-Mentor Learning Series Workshop 1</h1>
         <div className="user-info">
-          <p><strong>Name:</strong> Your Name</p> 
-          <p><strong>USN:</strong> Your USN</p>
+          <p><strong>Name:</strong> Vikas Padmashali</p> 
+          <p><strong>USN:</strong> 1RUA24CSE7021</p>
         </div>
       </header>
     </div>
